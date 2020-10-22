8447a726456a5a949b70f34a9ae9b14a39e95f6f
Miles Cohn <milesselim@icloud.com>
2020-10-22 19:36:05 -0400

    Added compression for better web speeds

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var compression = require('compression');
 var express = require("express");
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -10,6 +11,18 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
